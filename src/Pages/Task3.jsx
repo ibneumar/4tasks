90328e5b9a630a71538f656973a94bcff4fbc69f
Muhammad Adnan <ibn.e.umar.pk@gmail.com>
2023-11-26 00:57:44 +0500

    task 3

--- src/Pages/Task3.jsx
+++ src/Pages/Task3.jsx
@@ -1,9 +1,37 @@
 import React, {useState} from "react";
 
 const Task3 = () => {
-    return (
+    let [images, setImages] = useState([
+        "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-crunch-with-fries-and-drink-2023-05-31115706.png",
+        "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-chicken-n-chips-2023-05-31115706.png",
+        "https://www.kfcpakistan.com/images/43a98620-ffaa-11ed-b6b3-6970cc1cd666-zingerstack-Combo-2023-05-31115706.png",
+        "https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-family-Festivle-3-2023-05-31115706.png",
+        "https://www.kfcpakistan.com/images/43a95f10-ffaa-11ed-b673-4121381f04c6-CrispyDuoBox-2023-05-31115706.png"
+  ]);
+
+  let  [currentImage, setCurrentImage] = useState("https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-crunch-with-fries-and-drink-2023-05-31115706.png");
+  let [imgNumber, setImageNumber] = useState(1)
+    
+
+  const handleImage = (index) => {
+    setCurrentImage(images[index])
+    setImageNumber(index + 1)
+  }
+  return (
         <>
-            Task 3
+            <div className="container">
+            <div className="currentImage">
+                <img src={currentImage} alt="img" />
+            </div>
+            <div className="imagesBox">
+                {images.map((img, index) => (
+                    <img onClick={(e) => handleImage(index)} src={img} key={index} alt={`img${index}`}/>
+                ))}
+            </div>
+            
+                <h1>Showing {imgNumber} of {images.length} </h1>
+            </div>
+            
         </>
     )
 }
