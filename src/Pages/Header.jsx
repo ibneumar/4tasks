202117b03e87229e553b5c58b4397456a1d79f15
Muhammad Adnan <ibn.e.umar.pk@gmail.com>
2023-11-29 10:59:19 +0500

    nav

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -6,10 +6,10 @@ const Header = ({counter, total, resetCart}) => {
             <nav className="navbar navbar-expand-lg sticky-top bg-primary">
                 <div className="container">
                     <a className="navbar-brand text-light" href="#"><i className="bi bi-shop fz-30"></i> Store</a>
-                    <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    </button> */}
+                    {/* <div className="collapse navbar-collapse" id="navbarNavDropdown"> */}
                     <ul className="navbar-nav ms-auto relative">
                         <li className="nav-item">
                         <a className="nav-link text-light"><i className="bi bi-cart3 fz-20"></i><span className='counter'>{counter}</span></a>
@@ -21,7 +21,7 @@ const Header = ({counter, total, resetCart}) => {
                         <a className="nav-link text-light" title="Reset Cart" onClick={resetCart}><i className="bi bi-recycle fz-20"></i></a>
                         </li>
                     </ul>
-                    </div>
+                    {/* </div> */}
                 </div>
             </nav>
         </>
